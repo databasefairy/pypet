7cb4852cfdfeb89b98a565ef39b5f7c0dab8e3bd
databasefairy <nehollis@gmail.com>
2019-02-22 16:11:18 -0500

    Pypet feeding

--- pypet.py
+++ pypet.py
@@ -1,7 +1,18 @@
-name = 'Sal'
-age = 5
-weight = 10.5
-hungry = False
-photo = '(=^o.o^=)__'
 
-print 'Hi ' + name + photo
+cat = {
+    'name': 'Sal',
+    'hungry': True,
+    'weight': 10.5,
+    'photo': '(=^o.o^=)__'
+}
+
+def feed(pet):
+    if pet['hungry']==True:
+        pet['hungry'] = False
+        pet['weight'] = pet['weight'] + 1
+    else: 
+        print pet['name']+' is not hungry'
+
+print cat
+feed(cat)
+print cat
